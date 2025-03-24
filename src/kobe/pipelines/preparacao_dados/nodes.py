8fa06eafd0f11306b50ef19ff44ed01e5ae1495b
rosana <rosanariblim@gmail.com>
2025-03-24 12:57:33 -0300

    prepare_data, feature_engineering e separacao treino teste

--- src/kobe/pipelines/preparacao_dados/nodes.py
+++ src/kobe/pipelines/preparacao_dados/nodes.py
@@ -4,20 +4,48 @@ generated using Kedro 0.19.12
 """
 import mlflow
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
 
 def prepare_data(raw_dev):
 
-    mlflow.log_param("num_linhas", len(raw_dev))
+    raw_data_unique = raw_dev.drop_duplicates()
 
-    # Processamento dos dados
-    processed_data = raw_dev.dropna()
-    
+    data = (
+    raw_data_unique[['lat', 'lon','minutes_remaining','period','playoffs','shot_distance', 'shot_made_flag']]
+    .dropna()
+    .assign(playoffs = lambda x: x['playoffs'].astype(bool))
+    .assign(shot_made_flag = lambda x: x['shot_made_flag'].astype(bool))
+    )
+
     # Registrar métricas
-    mlflow.log_metric("linhas_antes", len(raw_dev))
-    mlflow.log_metric("linhas_depois", len(processed_data))
+    mlflow.log_metric("num_linhas_raw", len(raw_dev))
+    mlflow.log_metric("num_linhas_primary", len(data))
 
     # Salvar um artefato
-    processed_data.to_csv("processed_data.csv", index=False)
-    mlflow.log_artifact("processed_data.csv")
+    data.to_csv("primary_dev.csv", index=False)
+    mlflow.log_artifact("primary_dev.csv")
 
-    return processed_data
+    return data
+
+def feature_engineering(data):
+
+   # a análise para chegar nesses parâmetros foi feita no notebook feature_engineering.ipynb
+   data['lat_quadra'] = data['lat'] - 34.0443
+   data['lon_quadra'] = data['lon'] + 118.2698
+   data = data.drop(columns=["lat", "lon"])
+   
+   return data
+
+def separacao_treino_teste(data, random_state_param):
+
+    x_data = data.drop('shot_made_flag', axis=1)
+    y_data = data[['shot_made_flag']]
+
+    x_data_train, x_data_test, y_data_train, y_data_test = train_test_split(x_data, y_data, test_size=0.2,
+    random_state=random_state_param, stratify=y_data)
+
+    data_train = pd.concat([x_data_train, y_data_train], axis=1)
+    data_test = pd.concat([x_data_test, y_data_test], axis=1)
+
+    return data_train, data_test
--- src/kobe/pipelines/preparacao_dados/pipeline.py
+++ src/kobe/pipelines/preparacao_dados/pipeline.py
@@ -8,11 +8,22 @@ from . import nodes
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
-
          node(
             nodes.prepare_data,
             inputs="raw_dev",
-            outputs="feature_dev",
+            outputs="primary_dev",
             name="process_data_node"
+        ),
+        node(
+            nodes.feature_engineering,
+            inputs="primary_dev",
+            outputs="feature_dev",
+            name="feature_engineering_node"
+        ),
+        node(
+            nodes.separacao_treino_teste,
+            inputs=["feature_dev", "params:stratified_random_state"],
+            outputs=["data_train", "data_test"],
+            name="separacao_treino_teste_node"
         )
     ])
