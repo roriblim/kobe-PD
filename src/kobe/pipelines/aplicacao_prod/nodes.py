42178ca143319dbe058e0974dbcb81d69b97f351
rosana <rosanariblim@gmail.com>
2025-04-01 22:17:55 -0300

    continuando a refatoracao - predict de prod direto do modelo

--- src/kobe/pipelines/aplicacao_prod/nodes.py
+++ src/kobe/pipelines/aplicacao_prod/nodes.py
@@ -5,6 +5,28 @@ generated using Kedro 0.19.12
 
 
 
+def pre_process(data):
+
+    return None
+
 def teste(model):
 
     return None
+
+
+
+def get_f1_and_log_loss_and_predictions(data_prod, randcv_model):
+    
+    test_predictions = exp.predict_model(randcv_model, data=data_test)
+    test_predicted_target = test_predictions['prediction_label'].astype(int)
+
+    test_probs = randcv_model.predict_proba(data_test.drop(columns=["shot_made_flag"]))
+    test_actual_target = data_test['shot_made_flag'].astype(int)
+
+    # Log Loss
+    test_log_loss = log_loss(test_actual_target, test_probs[:, 1])
+    
+    # F1 score
+    test_f1 = f1_score(test_actual_target, test_predicted_target)
+
+    return test_log_loss, test_f1,test_predicted_target, test_probs
